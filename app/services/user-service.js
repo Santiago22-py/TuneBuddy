b0abb63028d5f7d9702ef94211657611bae074b9
Santiago22-py <joaovitor.santiagovianamelo@edu.sait.ca>
2025-12-11 02:32:17 -0700

    Fixed multiple profiles being created without checking it they already exist

--- app/services/user-service.js
+++ app/services/user-service.js
@@ -1,9 +1,15 @@
 import { db } from "../utils/firebase";
-import { doc, setDoc, serverTimestamp } from "firebase/firestore";
+import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
 
 // Function to create a user profile in Firestore
 export async function createUserProfile(user, displayName) {
   const userRef = doc(db, "users", user.uid);
+  const userSnap = await getDoc(userRef);
+
+  // If user profile doesn't exist, create it
+  if (userSnap.exists()) {
+    return;
+  }
 
   await setDoc(userRef, {
     username: displayName,
